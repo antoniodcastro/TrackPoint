34f6bf4666bd1f836c542bb1bfad185c6ed86e95
Antônio Castro <antoniolucas323@gmail.com>
2025-04-05 11:34:22 -0300

    feat: add scroll_manager to core module
    - Added `scroll_manager` to `core/__init__.py`
    - Created dedicated script to manage scroll behavior

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,3 +1,4 @@
 from .log_manager import LogManager
 from .windows_controller import WindowController 
+from .scroll_manager import ScrollManager
 import core.log as log
---
+++ src/core/scroll_manager.py
@@ -0,0 +1,68 @@
+from core import log
+import pyautogui as py
+from time import sleep
+
+class ScrollManager:
+    def __init__(self, window_controller):
+        
+        # Gerenciador de scroll
+        """
+        Args:
+            window_controller (WindowController): Controlador de janela
+        """
+        self.window_controller = window_controller
+        self._valor_scroll = 0
+        self._historico_scrolls = []
+
+    def set_valor(self, valor):
+
+        # Define o valor de scroll
+        """
+        Args:
+            valor (int): Valor do scroll
+        """
+        self._valor_scroll = valor
+        self._historico_scrolls.append(valor)
+
+    def get_valor(self):
+
+        # Obtém o valor atual de scroll
+        """
+        Returns:
+            int: Valor de scroll
+        """
+        return self._valor_scroll
+
+    def executar_scroll_teste(self):
+        # Executa o scroll 
+
+        # Log de início do scroll
+        log.add_log('INFO', 'Foi Clicado para testar o scroll')
+        
+        # Obtém o tempo de espera do log
+        numero = log.ler_numero_primeira_linha(log.app_state)
+        
+        # Minimiza a janela
+        self.window_controller.minimize()
+        
+        # Tempo de espera
+        sleep(numero)
+        
+        # Executa o scroll
+        py.scroll(self._valor_scroll)
+        
+        # Log do scroll
+        log.add_log('INFO', f'Foi testado um scroll de {self._valor_scroll}')
+        
+        # Tempo adicional
+        sleep(2)
+        
+        # Maximiza a janela
+        self.window_controller.maximize()
+
+    def salvar_scroll(self):
+        """
+        Salva o valor atual de scroll no log
+        """
+        log.add_log('INFO', 'Foi clicado para salvar scroll')
+        log.add_log_app('[Scroll]:', f"{self._valor_scroll}")
