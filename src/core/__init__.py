3653fd3a2239b3d84e1c0aadca5cbd21fae6974e
Antônio Castro <antoniolucas323@gmail.com>
2025-05-08 09:13:22 -0300

    feat: merge detect-platform branch into main with OS detection support

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,6 +1,7 @@
 from .log_manager import LogManager
 from .windows_controller import WindowController 
 from .scroll_manager import ScrollManager
+from .detect_platform  import detect
 from .mouse_manager import MousePositionManager
 from .monito_resol import MonitorResolucao
 from .log import criar_log,escrever_log,add_log,log_file,add_log_app,criar_app_state,ler_numero_primeira_linha
---
+++ src/core/detect_platform.py
@@ -0,0 +1,18 @@
+import flet as ft 
+from .log import add_log
+def detect (page: ft.Page):
+
+    if page.platform == ft.PagePlatform.LINUX:
+        add_log('INFO', 'Aplicativo foi iniciado aparte de um dispositivo Linux')
+        page.window.min_width = 700    
+        page.window.min_height = 500   
+        page.window.max_width = 800   
+        page.window.max_height = 500   
+
+    elif page.platform == ft.PagePlatform.WINDOWS:
+        add_log('INFO', 'Aplicativo foi iniciado aparte de um dispositivo Windows')
+        page.window.resizable = False  
+        page.window.maximizable = True 
+
+    else:
+        add_log('INFO', 'Adcionar Sua nova sistema.')
